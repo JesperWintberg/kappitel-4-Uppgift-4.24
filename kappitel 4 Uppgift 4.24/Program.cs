6a26c07eecabc71cd43c4b0701a4f762cdee7c2f
JesperWintberg <111962418+JesperWintberg@users.noreply.github.com>
2022-10-19 20:22:24 +0200

    Add project files.

---
+++ kappitel 4 Uppgift 4.24/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            string put;
+            for (int i = 1; i<= 100; i++)
+
+                 {
+                put = i.ToString();
+                if (i % 3 == 0)
+                {
+                    put = "Fizz";
+                }
+               else if (i % 5 == 0)
+                {
+                    put = "Buzz";
+                }
+                if (i % 3 == 0 && i % 5 ==0)
+                {
+                    put = "Buzz Fizz";
+                }
+
+
+                Console.WriteLine(put);
+            }  
+
+        }
+    }
+}
+
